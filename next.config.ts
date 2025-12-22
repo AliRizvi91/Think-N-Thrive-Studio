aaf77d7d8cf0db385089647783766ace31ca4a92
Ali Raza Attari <attaraliraza4@gmail.com>
2025-12-22 21:06:20 +0500

    Try For Better

--- next.config.ts
+++ next.config.ts
@@ -15,20 +15,20 @@ const nextConfig = {
     ],
   },
 
-  async rewrites() {
+  async redirects() {
     return [
-      { source: "/", destination: "/with-nav-footer/home" },
-      { source: "/about", destination: "/with-nav-footer/about" },
-      { source: "/contact", destination: "/with-nav-footer/contact" },
-      { source: "/courses", destination: "/with-nav-footer/courses" },
-      { source: "/admission", destination: "/with-nav-footer/admission" },
-      { source: "/authenticate", destination: "/with-nav-footer/authenticate" },
-      { source: "/course/:id", destination: "/with-nav-footer/course/:id" },
-      { source: "/verification", destination: "/no-nav-footer/verify-email" },
-      { source: "/profile", destination: "/with-nav-footer/profile" },
-      { source: "/details", destination: "/with-nav-footer/details" },
-      { source: "/faqs", destination: "/with-nav-footer/faqs" },
-      { source: "/admin-course", destination: "/with-nav-footer/adminCourse" },
+      { source: "/", destination: "/with-nav-footer/home", permanent: false },
+      { source: "/about", destination: "/with-nav-footer/about", permanent: false },
+      { source: "/contact", destination: "/with-nav-footer/contact", permanent: false },
+      { source: "/courses", destination: "/with-nav-footer/courses", permanent: false },
+      { source: "/admission", destination: "/with-nav-footer/admission", permanent: false },
+      { source: "/authenticate", destination: "/with-nav-footer/authenticate", permanent: false },
+      { source: "/course/:id", destination: "/with-nav-footer/course/:id", permanent: false },
+      { source: "/verification", destination: "/no-nav-footer/verify-email", permanent: false },
+      { source: "/profile", destination: "/with-nav-footer/profile", permanent: false },
+      { source: "/details", destination: "/with-nav-footer/details", permanent: false },
+      { source: "/faqs", destination: "/with-nav-footer/faqs", permanent: false },
+      { source: "/admin-course", destination: "/with-nav-footer/adminCourse", permanent: false },
     ];
   },
 };
